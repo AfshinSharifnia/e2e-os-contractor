b0c9a7953f390c8da46bf86ce549d233eed799fa
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-07-25 15:19:50 -0600

    signin test with POM

--- tests/2.SignIn.spec.js
+++ tests/2.SignIn.spec.js
@@ -1,18 +1,27 @@
 import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/logIn";
 
 //Recruiter
 test("SignIn Recruiter", async ({ page }) => {
-  await page.goto("https://os-project-black.vercel.app/");
-  await page.locator("text='Log In Or Sign Up'").click();
-  await page.locator("#emailInput").fill("rec1@test.com");
-  await page.locator("[placeholder='Password...']").fill("Rec2023$");
-  await page.locator("text='Login'").click();
+  const testLogin = new LoginPage(page);
+  await testLogin.gotoLoginPage();
+  await testLogin.login("rec1@test.com", "Rec2023$");
+
+  // await page.goto("https://os-project-black.vercel.app/");
+  // await page.locator("text='Log In Or Sign Up'").click();
+  // await page.locator("#emailInput").fill("rec1@test.com");
+  // await page.locator("[placeholder='Password...']").fill("Rec2023$");
+  // await page.locator("text='Login'").click();
 });
+
 //Contractor
 test("SignIn Contractor", async ({ page }) => {
-  await page.goto("https://os-project-black.vercel.app/");
-  await page.locator("text='Log In Or Sign Up'").click();
-  await page.locator("#emailInput").fill("cont1@test.com");
-  await page.locator("[placeholder='Password...']").fill("Cont2023$");
-  await page.locator("text='Login'").click();
+  const testLogin = new LoginPage(page);
+  await testLogin.gotoLoginPage();
+  await testLogin.login("cont1@test.com", "Cont2023$");
+  // await page.goto("https://os-project-black.vercel.app/");
+  // await page.locator("text='Log In Or Sign Up'").click();
+  // await page.locator("#emailInput").fill("cont1@test.com");
+  // await page.locator("[placeholder='Password...']").fill("Cont2023$");
+  // await page.locator("text='Login'").click();
 });
